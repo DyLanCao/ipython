0cb74001e101644f67ae3ecd27d6388da025ed0c
cao yin <caoyin2011@163.com>
2019-07-27 17:02:02 +0800

    add scripts on it

---
+++ scripts/log_test1.py
@@ -0,0 +1,31 @@
+# python3
+import logging
+
+import logging.config
+
+def main():
+	'''
+	logging.basicConfig(
+	filename='app.log',
+	level=logging.ERROR,
+	format='%(levelname)s:%(asctime)s:%(message)s')
+	'''
+	logging.config.fileConfig('config/config.ini')
+	logging.getLogger("root")
+
+	#variables 
+	hostname = 'www.python.org'
+	item = 'spam'
+	filename = 'data.csv'
+	mode = 'r'
+
+	# example logging calls
+	logging.critical('Host %s unknown', hostname)
+	logging.error("Couldn't find %r",item)
+	logging.warning('Feature is deprecated')
+	logging.info('Opening file %r, mode=%r', filename,mode)
+	logging.debug('Got here')
+
+
+if __name__ == '__main__':
+	main()
